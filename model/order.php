4f9794f8df943d7b255bf528d7abfa0cc5223c9a
PETEL Noa <petel.noa@gmail.com>
2024-01-14 16:14:37 +0100

    Hotfix : commandes ne se mélange plus, admin fix

--- model/order.php
+++ model/order.php
@@ -4,8 +4,6 @@ require_once "UserModel.php";
 function saveOrder($type)
 {
 
-    session_start();
-
     // Calcul total panier
     $total = 0;
     if (isset($_SESSION['cart'])) {
@@ -24,65 +22,68 @@ function saveOrder($type)
         $stmt = $db->prepare("INSERT INTO orders (customer_id, registered, delivery_add_id, payment_type, date, status, session, total) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
 
         if (isset($_SESSION['user'])) {
-            $stmt->execute([$_SESSION['user']['id'], 1, 0, $type, date('Y-m-d'), 0, "session", $total]);
+            $userId = $_SESSION['user']['id'];
         } else {
-
-            // Creation d'un id client virtuel
-            $usermodel = new UserModel();
-            $usermodel->saveCustomer(session_id(), "none", "none", "none", "none", $_SESSION['adress'][1], $_SESSION['adress'][0], $_SESSION['adress'][2]);
-
-            // Récupération de l'id de l'utilisateur virtuel 
-            $sessionId = session_id();
-            $query = $db->prepare("SELECT id FROM customers WHERE surname = :userName");
-            $query->bindParam(':userName', $sessionId, PDO::PARAM_STR);
-            $query->execute();
-            $id = $query->fetchAll();
-
-            $id = $id[0]['id'];
-            
-            $stmt->execute([$id, 0, 0, $type, date('Y-m-d'), 0, "", $total]);
+            $userId = createVirtualId($type, $total, $db, $stmt);
         }
+        $stmt->execute([$userId, 1, 0, $type, date('Y-m-d'), 0, "session", $total]);
 
-        // Ajout de chaque produit dans la BD
+        // Récupération de l'id commande 
         if (isset($_SESSION['user'])) {
             $userId = $_SESSION['user']['id'];
-            $stmt = $db->prepare("SELECT id FROM orders WHERE customer_id = :userId");
-            $stmt->bindParam(':userId', $userId, PDO::PARAM_INT);
-            $stmt->execute();
-            $id = $stmt->fetch();
-            $id = $id['id'];
-        } else {
-            // Récupération de l'id de de l'utilisateur virtuel
-            $sessionId = session_id();
-            $query = $db->prepare("SELECT id FROM customers WHERE surname = :userName");
-            $query->bindParam(':userName', $sessionId, PDO::PARAM_STR);
-            $query->execute();
-            $id = $query->fetchAll();
-            
-            $id = $id[0]['id'];
-            print_r($id);   
-
-            $stmt = $db->prepare("SELECT id FROM orders WHERE customer_id = :userId");
-            $stmt->bindParam(':userId', $id, PDO::PARAM_INT);
-            $stmt->execute();
-            $id = $stmt->fetch();
-            $id = $id['id'];
-            
         }
+        $stmt = $db->prepare("SELECT id FROM orders WHERE customer_id = :userId ORDER BY id DESC");
+        $stmt->bindParam(':userId', $userId, PDO::PARAM_INT);
+        $stmt->execute();
+        $orderId = $stmt->fetch();
+        $orderId = $orderId['id'];
+        print_r($orderId);
 
         if (isset($_SESSION['cart'])) {
             foreach ($_SESSION['cart'] as $product) {
-                
+
 
                 $stmt = $db->prepare("INSERT INTO orderitems (order_id, product_id, quantity)  VALUES (?, ?, ?) ");
-                $stmt->execute([$id, $product['id'], $product['quantity']]);
+                $stmt->execute([$orderId, $product['id'], $product['quantity']]);
             }
         }
 
         // On vide la cart
-            unset($_SESSION['cart']);
+        unset($_SESSION['cart']);
 
     } catch (Exception $e) {
         echo ("Problem PDO" . $e->getMessage());
+    }
+}
+
+function createVirtualId($type, $total, $db, $stmt)
+{
+    $sessionId = session_id();
+
+    $query = $db->prepare("SELECT id FROM customers WHERE surname = :userName");
+    $query->bindParam(':userName', $sessionId, PDO::PARAM_STR);
+    $query->execute();
+    $id = $query->fetch();
+    $id = $id['id'];
+
+
+    if (empty($id)) {
+        // Creation d'un id client virtuel
+        $usermodel = new UserModel();
+        $usermodel->saveCustomer(session_id(), "none", "none", "none", "none", $_SESSION['adress'][1], $_SESSION['adress'][0], $_SESSION['adress'][2]);
+
+        // Récupération de l'id de l'utilisateur virtuel 
+
+        $query = $db->prepare("SELECT id FROM customers WHERE surname = :userName");
+        $query->bindParam(':userName', $sessionId, PDO::PARAM_STR);
+        $query->execute();
+        $id = $query->fetchAll();
+
+        $id = $id[0]['id'];
+
+        $stmt->execute([$id, 0, 0, $type, date('Y-m-d'), 0, "", $total]);
+        return $id;
+    } else {
+        return $id;
     }
 }
